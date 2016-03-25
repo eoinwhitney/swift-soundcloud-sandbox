8a051338743c0a01b03df0288b1b04626a5501af
eoinwhitney <whitneyeoin@gmail.com>
2016-03-25 11:14:07 -0400

    secret stuff

---
+++ SoundcloudTestApp/MeSecrets.swift
@@ -0,0 +1,15 @@
+//
+//  MeSecrets.swift
+//  SoundcloudTestApp
+//
+//  Created by Janet Lustgarten on 3/25/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import Foundation
+
+let SoundcloudClientID = "b24588e9cb3c38ca4c9986df1e3c1ef1"
+let SoundcloudClientSecret = "501b6b9263fb0a4fe8fa37e0987a6d42"
+
+let SoundcloudUsername = "user-213285123"
+let SoundcloudPassword = "Fall2010"
